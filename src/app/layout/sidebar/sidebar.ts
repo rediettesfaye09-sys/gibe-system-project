96f396ee28473e33f0d8087f8bf22d12be6872c0
Rediet Tesfaye <rediettesfaye09@gmail.com>
2026-05-28 21:54:33 +0300

    Add toggle functionality for user management sidebar menu

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  isUserManagementOpen: boolean = true;
+
+  toggleUserManagement(): void {
+    this.isUserManagementOpen = !this.isUserManagementOpen;
+  }
+}
