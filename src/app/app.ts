5638f5d5fff3f7cf8227c0ea61e77d7f34519ccd
Rediet Tesfaye <rediettesfaye09@gmail.com>
2026-05-30 10:47:23 +0300

    feat(navbar): create navbar component structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './layout/sidebar/sidebar';
 import { CommonModule } from '@angular/common';
+import { Navbar } from './layout/navbar/navbar';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, CommonModule],
+  imports: [RouterOutlet, Sidebar, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
