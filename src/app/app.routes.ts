8d5f1072a27347fb3da44e7a0acaf2cc6aea1d56
Rediet Tesfaye <rediettesfaye09@gmail.com>
2026-06-02 22:26:35 +0300

    feat(self-service): add self-service portal component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {Dashboard} from "./pages/dashboard/dashboard";
+import {SelfService} from "./pages/self-service/self-service";
 
 export const routes: Routes = [
     {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    {path: 'dashboard', component: Dashboard}
+    {path: 'dashboard', component: Dashboard},
+    {path: 'self-service', component: SelfService}
 ];
---
+++ src/app/pages/self-service/self-service.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-self-service',
+  imports: [],
+  templateUrl: './self-service.html',
+  styleUrl: './self-service.css',
+})
+export class SelfService {}
